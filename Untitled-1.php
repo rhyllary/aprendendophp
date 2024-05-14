7dfa7d69c37e9e490ec84240a33751b69f9b977b
rhyllary <rhyllaryvitoria023@email.com>
2024-05-14 17:08:57 -0300

    commit

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,26 +1,15 @@
+<!DOCTYPE html>
+</head>
+<body>
 <?php
-   $nome ="Rodrigo";
-   $dia = 11;
-   echo $nome;
-    echo $dia;
-    $diaext = "7";
-
-    echo $diaext;
-    if($dia == $diaext){
-        echo "igual";
-    }
-
-    echo " testando se é igual a 7<br>";
-    if($dia != 7)
-    {
-        echo "diferente1";
-    }
-    else {
-        echo "igual a 7";
-    }
     
-    echo "<br> Operador ternario<br>";
-    $resposta = $dia < 10 ? "Menor que 10" : "Maior ou igual";
-    echo $resposta;
-
+   $nome = "rhyllary";
+  $numero = 8;
+ if ($numero >= 100) 
+ {
+    ECHO "seu nomero é maior "
+ }
     ?>
+</body>
+</html>
+
